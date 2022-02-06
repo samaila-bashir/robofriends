0c91ed2ba7caa45ce2da7c6631e3aa32446a9b6d
Samaila Chatto Bashir <chattobashir@gmail.com>
2022-02-06 19:11:32 +0100

    Reoptimized codebase and reorganized folders

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import CardList from "./CardList";
-import SearchBox from "./SearchBox";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
 import './App.css';
-import Scroll from './Scroll';
+import Scroll from '../components/Scroll';
 
 class App extends Component {
     constructor() {
@@ -24,11 +24,13 @@ class App extends Component {
     }
 
     render() {
-        const filteredRobots = this.state.robots.filter(robot => {
+        const { robots, searchfield } = this.state;
+
+        const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
 
-        if (this.state.robots.length === 0)
+        if (robots.length === 0)
         {
             return <h1>Loading...</h1>
         } else {
